1ffed13f2445107e6d47251bf131a660d24cfe69
charlesbintang <charlesbintang203@gmail.com>
2024-02-05 03:11:24 +0700

    Complete UI design : campaign container

--- lib/pages/community/pages/c_home_page_view.dart
+++ lib/pages/community/pages/c_home_page_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
 import 'package:lautkita_mobile/pages/community/widget/search_bar_widget.dart';
 
 class CHomePageView extends StatelessWidget {
@@ -33,7 +35,168 @@ class CHomePageView extends StatelessWidget {
                   )),
             ),
           ],
-        )
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(40.0, 30.0, 40.0, 0),
+          sliver: SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  clipBehavior: Clip.hardEdge,
+                  height: 260.h,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 1),
+                        blurRadius: 1,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      // gambar
+                      SizedBox(
+                        height: 110.h,
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.network(
+                          "https://img.freepik.com/free-photo/breathtaking-shot-beautiful-stones-turquoise-water-lake-hills-background_181624-12847.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      // teks
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 5, 5, 5),
+                        child: Column(
+                          children: [
+                            // judul campaign dan 3 dot
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Ocean Healty",
+                                  style: TextStyle(
+                                    color: Color(0xFF364C6F),
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Icon(Icons.more_vert, color: Color(0xFF98A2B3)),
+                              ],
+                            ),
+                            // deskripsi
+                            Padding(
+                              padding: EdgeInsets.only(right: 5),
+                              child: Text(
+                                "Lorem ipsum dolor sit amet consectetur. Amet maecenas.Viverra mi euismod dinamik mi euismod",
+                                style: TextStyle(
+                                  color: Color(0xFF667085),
+                                ),
+                              ),
+                            ),
+                            // statistik dan tombol
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Raised of ",
+                                          style: TextStyle(
+                                            color: Color(0xFF2F80ED),
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                        Text(
+                                          "15%",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          padding: EdgeInsets.all(4),
+                                          margin: EdgeInsets.only(right: 5),
+                                          decoration: BoxDecoration(
+                                            color: Color(0xFFE6EEFA),
+                                            borderRadius:
+                                                BorderRadius.circular(4),
+                                          ),
+                                          child: Text(
+                                            "Raise Goal",
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              fontWeight: FontWeight.w500,
+                                              color: Color(0xFF2F80ED),
+                                            ),
+                                          ),
+                                        ),
+                                        Text(
+                                          NumberFormat.currency(
+                                            locale: 'id_ID',
+                                            symbol: 'Rp',
+                                            decimalDigits: 0,
+                                          ).format(800000),
+                                          style: TextStyle(
+                                            color: Color(
+                                              0xFF9EAEC8,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                const Expanded(child: SizedBox()),
+                                Expanded(
+                                  flex: 8,
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Text("DETAIL"),
+                                    style: ButtonStyle(
+                                      foregroundColor: MaterialStatePropertyAll(
+                                          Colors.white),
+                                      backgroundColor: MaterialStatePropertyAll(
+                                        Color(0xFF3EB5E8),
+                                      ),
+                                      shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const Expanded(child: SizedBox()),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ),
       ],
     );
   }
