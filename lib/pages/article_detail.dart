ec8b762be4dafc1ceb10ca5daf2ca5d1bb833bed
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2024-02-17 16:39:33 +0700

    integrate detail article with api

--- lib/pages/article_detail.dart
+++ lib/pages/article_detail.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
+import '../common/global_variables.dart';
+import '../data/models/article_response_model.dart';
 
 class ArticleDetail extends StatefulWidget {
-  const ArticleDetail({super.key});
+  final Post post;
+  const ArticleDetail({Key? key, required this.post}) : super(key: key);
 
   @override
   State<ArticleDetail> createState() => _ArticleDetailState();
 }
 
 class _ArticleDetailState extends State<ArticleDetail> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +28,11 @@ class _ArticleDetailState extends State<ArticleDetail> {
               child: Container(
                 width: 375.w,
                 height: 405.h,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   image: DecorationImage(
                     fit: BoxFit.cover,
-                    image: AssetImage(
-                      "assets/images/bg-artikel.png",
+                    image: NetworkImage(
+                      '${GlobalVariables.baseUrl}${widget.post.image}',
                     ),
                   ),
                 ),
@@ -40,7 +47,9 @@ class _ArticleDetailState extends State<ArticleDetail> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           BackButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.maybePop(context);
+                            },
                             color: Colors.white,
                           ),
                           Column(
@@ -68,50 +77,70 @@ class _ArticleDetailState extends State<ArticleDetail> {
                         flex: 1,
                         child: SizedBox(),
                       ),
-                      const Expanded(
+                      Expanded(
                         flex: 2,
                         child: Text(
-                          "Lorem Ipsum is simply dummy\ntext of the printing",
-                          style: TextStyle(
+                          widget.post.title!,
+                          style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                             fontSize: 22,
                           ),
                         ),
                       ),
-                      const Expanded(
+                      Expanded(
                         flex: 3,
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(horizontal: 0),
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                AssetImage("assets/images/Ellipse5.png"),
-                            radius: 35,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                offset: const Offset(0, 2),
+                                blurRadius: 3,
+                                spreadRadius: 25,
+                              ),
+                            ],
                           ),
-                          title: Text(
-                            "John Doe",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                          subtitle: Text(
-                            "Writer",
-                            style: TextStyle(
-                              color: Color(0xFFACAFC3),
-                              fontSize: 18,
+                          child: ClipRect(
+                            child: ListTile(
+                              contentPadding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                              leading: const CircleAvatar(
+                                backgroundImage:
+                                    AssetImage("assets/images/avatarman.png"),
+                                radius: 35,
+                              ),
+                              title: Text(
+                                widget.post.author?.name ?? '',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              subtitle: const Text(
+                                "Writer",
+                                style: TextStyle(
+                                  color: Color(0xFFACAFC3),
+                                  fontSize: 18,
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
               ),
             ),
+
+            // Bagian bawah dengan konten artikel
             Positioned(
               bottom: 0,
+              left: 0,
+              right: 0,
               child: Container(
                 width: 375.w,
                 height: 444.h,
@@ -122,32 +151,27 @@ class _ArticleDetailState extends State<ArticleDetail> {
                     topRight: Radius.circular(30),
                   ),
                 ),
-                child: const Padding(
-                  padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 8.0),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 8.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "Results\n",
-                        style: TextStyle(
+                        widget.post.category?.name ?? '',
+                        style: const TextStyle(
                           fontSize: 16,
                           color: Color(0xFF333647),
                           fontWeight: FontWeight.bold,
                         ),
                       ),
+                      const SizedBox(
+                        height: 16.0,
+                      ),
                       Expanded(
                         child: SizedBox(
                           child: SingleChildScrollView(
-                            child: Text(
-                              "Leads in individual states may change from one party to another as all the votes are counted. Select a state for detailed results, and select the Senate, House or Governor tabs to view those races.\n\n"
-                              "For more detailed state results click on the States A-Z links at the bottom of this page. Results source: NEP/Edison via Reuters.\n\n"
-                              "Leads in individual states may change from one party to another as all the votes are counted. Select a state for detailed results, and select the Senate, House or Governor tabs to view those races.\n\n"
-                              "For more detailed state results click on the States A-Z links at the bottom of this page. Results source: NEP/Edison via Reuters.\n\n",
-                              style: TextStyle(
-                                fontSize: 17,
-                                color: Color(0xFF666C8E),
-                                height: 1.39,
-                              ),
+                            child: HtmlWidget(
+                              widget.post.body!,
                             ),
                           ),
                         ),
--- lib/pages/home/widgets/item_article_widget.dart
+++ lib/pages/home/widgets/item_article_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lautkita_mobile/data/models/article_response_model.dart';
+import 'package:lautkita_mobile/pages/article_detail.dart';
 
 import '../../../common/global_variables.dart';
 
@@ -13,54 +14,62 @@ class ArticleItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: FadeInImage.assetNetwork(
-                height: 96.0,
-                width: 96.0,
-                fit: BoxFit.cover,
-                image: '${GlobalVariables.baseUrl}${post.image}',
-                placeholder: 'assets/images/placeholder_1x1.png',
-                imageErrorBuilder: (c, o, s) => Image.asset(
-                  'assets/images/placeholder_1x1.png',
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return ArticleDetail(
+            post: post,
+          );
+        }));
+      },
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: FadeInImage.assetNetwork(
+                  height: 96.0,
+                  width: 96.0,
                   fit: BoxFit.cover,
-                  height: MediaQuery.of(context).size.width / 2.45,
+                  image: '${GlobalVariables.baseUrl}${post.image}',
+                  placeholder: 'assets/images/placeholder_1x1.png',
+                  imageErrorBuilder: (c, o, s) => Image.asset(
+                    'assets/images/placeholder_1x1.png',
+                    fit: BoxFit.cover,
+                    height: MediaQuery.of(context).size.width / 2.45,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(width: 16.0),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 8),
-                  Text(
-                    post.category!.name!,
-                    style: const TextStyle(
-                      fontSize: 14.0,
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8),
+                    Text(
+                      post.category!.name!,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 6),
-                  Text(
-                    post.title!,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(height: 6),
+                    Text(
+                      post.title!,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
